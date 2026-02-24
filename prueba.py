cd1c24b905a69097288f9715a9bd1111332e88dc
Josephe <jimenezalferezjoseph@gmail.com>
2026-02-24 04:02:10 -0700

    bo usar GUI

--- prueba.py
+++ prueba.py
@@ -3,12 +3,13 @@ import cv2
 import numpy as np
 import pyautogui
 import time
+import pydirectinput
 
 
 def ejecutar_accion():
     # mss es lo que toma la cpatura de pantalla
     with mss.mss() as sct:
-        time.sleep(10)
+        time.sleep(5)
         # Esto camiba que mointor es importnate saber el del marcos
         monitor = sct.monitors[1]
         # esto convertimos la captura en un arrelgo de numPy
@@ -30,31 +31,33 @@ def ejecutar_accion():
         if contornos:
             # Encontrar el contorno más grande (el marco)
             c = max(contornos, key=cv2.contourArea)
-
             # Obtener las coordenadas del centro
-            pyautogui.sleep(10)
+
             M = cv2.moments(c)
             if M["m00"] != 0:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
 
-                pyautogui.moveTo(cX, cY, duration=0.2)
+                pydirectinput.moveTo(cX, cY, duration=0.2)
 
-                pyautogui.leftClick()
-                pyautogui.leftClick()
-                pyautogui.sleep(10)
-                pyautogui.keyDown('up')
+                pydirectinput.leftClick()
+                pydirectinput.leftClick()
+                time.sleep(2)
+                pydirectinput.moveTo(cX+5, cY+5, duration=0.2)
+                pydirectinput.leftClick()
+                time.sleep(10)
+                pydirectinput.keyDown('up')
                 time.sleep(2)  # El tiempo que desees mantener la tecla
-                pyautogui.keyUp('up')
-                pyautogui.keyDown('down')
+                pydirectinput.keyUp('up')
+                pydirectinput.keyDown('down')
                 time.sleep(2)  # El tiempo que desees mantener la tecla
-                pyautogui.keyUp('down')
-                pyautogui.keyDown('left')
+                pydirectinput.keyUp('down')
+                pydirectinput.keyDown('left')
                 time.sleep(2)  # El tiempo que desees mantener la tecla
-                pyautogui.keyUp('left')
-                pyautogui.keyDown('right')
+                pydirectinput.keyUp('left')
+                pydirectinput.keyDown('right')
                 time.sleep(2)  # El tiempo que desees mantener la tecla
-                pyautogui.keyUp('right')
+                pydirectinput.keyUp('right')
         else:
             print("No se encontró ningún marco azul en la pantalla.")
 
